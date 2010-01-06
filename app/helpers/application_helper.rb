aa8afa4e0d5225e7b137dfb417d330e0aaa95350
pramod reddy <pramod.mittapalli@yahoo.com>
2010-01-06 13:29:17 +0530

    small changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+# Returns a title on per page basis
+  def title
+  base_title = "Ruby on rails sample app"
+    if @title.nil?
+      base_title
+    else
+     "#{base_title} | #{@title}"
+    end 
+  end
 end
